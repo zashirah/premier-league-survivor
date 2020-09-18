4c511cc928f47e2e622cc76e787fdbbe3ce7b5fe
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-09-18 14:20:09 -0400

    fixed migrate files

--- db/migrate/20200915231102_drop_match_table.rb
+++ db/migrate/20200915231102_drop_match_table.rb
@@ -1,5 +1,5 @@
 class DropMatchTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :matches
-  end
+  # def change
+  #   drop_table :matches
+  # end
 end
--- db/migrate/20200915231155_drop_team_table.rb
+++ db/migrate/20200915231155_drop_team_table.rb
@@ -1,5 +1,5 @@
 class DropTeamTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :teams
-  end
+  # def change
+  #   drop_table :teams
+  # end
 end
